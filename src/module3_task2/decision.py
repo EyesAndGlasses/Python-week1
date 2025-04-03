5502d7e8ba25f57b19f36e693f1d4cedae84ca41
Catherine2000 <katerinapanina@icloud.com>
2025-04-03 10:15:17 +0300

    Исправлено решение задачи. Теперь используется asyncio.Queue

--- src/module3_task2/decision.py
+++ src/module3_task2/decision.py
@@ -1,38 +1,57 @@
 import aiohttp
 import asyncio
 import json
+import os
 from aiofiles import open as aio_open
 
 
-async def fetch(url, semaphore):
-    async with semaphore:
+async def fetch(queue: asyncio.Queue, results: list):
+    while True:
+        url = await queue.get()
+        if url is None:
+            break
+
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get(
                     url, timeout=aiohttp.ClientTimeout(total=10)
                 ) as response:
-                    return {"url": url, "status": response.status}
+                    results.append({"url": url, "status": response.status})
         except aiohttp.ClientConnectionError:
-            return {"url": url, "status": 0}
+            results.append({"url": url, "status": 0})
+
         except asyncio.TimeoutError:
-            return {"url": url, "status": 0}
+            results.append({"url": url, "status": 0})
+
         except Exception as e:
-            return {"url": url, "status": 0, "error": str(e)}
+            results.append({"url": url, "status": 0})
+
+        queue.task_done()
 
 
 async def fetch_urls(input_file: str, output_file: str):
-    semaphore = asyncio.Semaphore(5)
+    queue = asyncio.Queue()
+    results = []
 
     async with aio_open(input_file, "r") as file:
         urls = [line.strip() for line in await file.readlines()]
 
-    tasks = [fetch(url, semaphore) for url in urls]
-    results = await asyncio.gather(*tasks)
+    for url in urls:
+        await queue.put(url)
+
+    num_workers = min(len(urls), os.cpu_count())
+
+    workers = [asyncio.create_task(fetch(queue, results)) for _ in range(num_workers)]
+
+    await queue.join()
+
+    for _ in range(num_workers):
+        await queue.put(None)
+    await asyncio.gather(*workers)
 
     async with aio_open(output_file, "w") as file:
         for result in results:
-            if result:
-                await file.write(json.dumps(result) + "\n")
+            await file.write(json.dumps(result) + "\n")
 
 
 if __name__ == "__main__":
