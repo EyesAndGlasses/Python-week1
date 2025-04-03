aa4ec9a333627a922c37b2c149a1e2820794328f
Catherine2000 <katerinapanina@icloud.com>
2025-04-03 10:28:44 +0300

    Исправлено: одна сессия на все запросы, ограничение до 5 вернула (убирала его в прошлом коммите)

--- src/module3_task2/decision.py
+++ src/module3_task2/decision.py
@@ -5,33 +5,36 @@ import os
 from aiofiles import open as aio_open
 
 
-async def fetch(queue: asyncio.Queue, results: list):
+async def fetch(
+    queue: asyncio.Queue,
+    semaphore: asyncio.Semaphore,
+    session: aiohttp.ClientSession,
+    results: list,
+):
     while True:
         url = await queue.get()
         if url is None:
             break
 
-        try:
-            async with aiohttp.ClientSession() as session:
+        async with semaphore:
+            try:
                 async with session.get(
                     url, timeout=aiohttp.ClientTimeout(total=10)
                 ) as response:
                     results.append({"url": url, "status": response.status})
-        except aiohttp.ClientConnectionError:
-            results.append({"url": url, "status": 0})
-
-        except asyncio.TimeoutError:
-            results.append({"url": url, "status": 0})
-
-        except Exception as e:
-            results.append({"url": url, "status": 0})
+            except (aiohttp.ClientConnectionError, asyncio.TimeoutError):
+                results.append({"url": url, "status": 0})
+            except Exception as e:
+                results.append({"url": url, "status": 0, "error": str(e)})
 
         queue.task_done()
 
 
 async def fetch_urls(input_file: str, output_file: str):
+    num_workers = 5
     queue = asyncio.Queue()
     results = []
+    semaphore = asyncio.Semaphore(num_workers)
 
     async with aio_open(input_file, "r") as file:
         urls = [line.strip() for line in await file.readlines()]
@@ -39,15 +42,17 @@ async def fetch_urls(input_file: str, output_file: str):
     for url in urls:
         await queue.put(url)
 
-    num_workers = min(len(urls), os.cpu_count())
+    async with aiohttp.ClientSession() as session:
+        workers = [
+            asyncio.create_task(fetch(queue, semaphore, session, results))
+            for _ in range(num_workers)
+        ]
 
-    workers = [asyncio.create_task(fetch(queue, results)) for _ in range(num_workers)]
+        await queue.join()
 
-    await queue.join()
-
-    for _ in range(num_workers):
-        await queue.put(None)
-    await asyncio.gather(*workers)
+        for _ in range(num_workers):
+            await queue.put(None)
+        await asyncio.gather(*workers)
 
     async with aio_open(output_file, "w") as file:
         for result in results:
